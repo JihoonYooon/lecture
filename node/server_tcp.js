31016d82ceca7ef3c2c3247b33b06359749fd1c9
JihoonYooon <dobbyisfr22@naver.com>
2021-06-17 12:42:34 +0900

    main

--- node/server_tcp.js
+++ node/server_tcp.js
@@ -1,18 +1,21 @@
 var net = require('net');
+
 var server = net.createServer(function(client) {
-  client.setTimeout(500);
-  client.setEncoding('utf8');
-  client.on('data', function(data) {
-    writeData(client, 'Sending: ' + data.toString());
-  });
-  client.on('end', function() {
-    server.getConnections(function(err, count){
-    });
-  });
-  client.on('error', function(err) {
-  });
-  client.on('timeout', function() {
-  });
+    console.log("Hi");
+    client.setTimeout(500);
+    client.setEncoding('utf8');
+    client.on('data', function(data) {
+        user = data.split("|")[0];
+        writeData(client, 'Sending: nice to meet you ' + user.toString());
+    });
+    client.on('end', function() {
+        server.getConnections(function(err, count){
+        });
+    });
+    client.on('error', function(err) {
+    });
+    client.on('timeout', function() {
+    });
 });
 
 server.listen(8107, function() {
@@ -20,6 +23,7 @@ server.listen(8107, function() {
   server.on('close', function(){
   });
   server.on('error', function(err){
+    console.log(err)
   });
 });
 function writeData(socket, data){
